618f31b102fc5f09178dc92b2a02b73d267f1cb7
Simone Federici <s.federici@gmail.com>
2011-11-02 02:34:02 +0100

    sample models

--- testprj/sample/models.py
+++ testprj/sample/models.py
@@ -13,15 +13,14 @@ class Book(models.Model):
 
 class OldBook(models.Model):
     id = models.CharField(max_length=200, primary_key=True)
-    #id = db.MultipleFieldPrimaryKey(fields=["author", "name"])
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
 
     def __unicode__(self):
         return u"%s (by %s)" % (self.name, self.author)
 
-#class RealBook(Book):
-#    text = models.CharField(max_length=100)
+class RealBook(Book):
+    text = models.CharField(max_length=100)
 
 class RealOldBook(OldBook):
     text = models.CharField(max_length=100)
@@ -31,7 +30,7 @@ class Library(models.Model):
     name = models.CharField(max_length=100)
 
 class Biografy(models.Model):
-#    id = db.MultipleFieldPrimaryKey(fields=["book",])
+    id = db.MultipleFieldPrimaryKey(fields=["book",])
     book = models.OneToOneField(Book)
     text = models.CharField(max_length=100)
 
@@ -47,12 +46,12 @@ class OldBiografy(models.Model):
         return u"BIO: %s" % unicode(self.book)
 
 class AbstractChapter(models.Model):
-#    id = db.MultipleFieldPrimaryKey(fields=["book", "number"])
+    id = db.MultipleFieldPrimaryKey(fields=["book", "number"])
     book = models.ForeignKey(Book, to_field="id",
-#                             fields_ext={
- #           "author": {"db_column" :"b_author", "name" : "_author"},
- #           "name"  : {"db_column" :"b_name"},
-#    },
+                             fields_ext={
+            "author": {"db_column" :"b_author", "name" : "_author"},
+            "name"  : {"db_column" :"b_name"},
+    },
     related_name="chapter_set")
     number = models.PositiveSmallIntegerField()
     title = models.CharField(max_length=100)
