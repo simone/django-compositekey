eb2d86f2c4a3016d213ba3fd4b83022d2fbe2631
Serafino Beraldi <serafino@D820-UBU.(none)>
2011-11-03 15:11:52 +0100

    Added test on book chapter inlines

--- testprj/sample/tests.py
+++ testprj/sample/tests.py
@@ -4,9 +4,11 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from django.contrib.auth.models import User
 from django.test import TestCase
 from compositekey.utils import *
 from sample.models import *
+from compositekey.utils import assemble_pk
 
 
 class ModelTest(TestCase):
@@ -109,3 +111,64 @@ class UtilsTest(TestCase):
         params.append(None)
         self.assertEquals(params, disassemble_pk(assemble_pk(*params)))
         self.assertEquals([None, '', 'TEST'], disassemble_pk(assemble_pk(None, '', 'TEST')))
+
+
+class AdminTest(TestCase):
+
+    fixtures = ['admin.json']
+
+    def setUp(self):
+        username = 'test_user'
+        pwd = 'secret'
+
+        self.u = User.objects.create_user(username, '', pwd)
+        self.u.is_staff = True
+        self.u.is_superuser = True
+        self.u.save()
+
+        self.assertTrue(self.client.login(username=username, password=pwd), "Logging in user %s, pwd %s failed." % (username, pwd))
+
+    def tearDown(self):
+        self.client.logout()
+        self.u.delete()
+
+    def test_book_chapter_inlines(self):
+        autore_libro = 'auore libro 1'
+        nome_libro = 'nome libro 1'
+        book_id = '%s-%s' % (autore_libro, nome_libro)
+
+        response = self.client.get('/admin/sample/book/%s/' % book_id)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(book_id, response.context['object_id'])
+        self.assertEqual("Change book", response.context['title'])
+
+
+        # Don't change nothing, simple try post data:
+
+        post_data = {
+            'name': nome_libro,
+            'author': autore_libro ,
+            'chapter_set-TOTAL_FORMS': u'3',
+            'chapter_set-INITIAL_FORMS': u'0',
+            'chapter_set-MAX_NUM_FORMS': u'',
+
+            'chapter_set-0-book': book_id,
+            'chapter_set-0-number': u'',
+            'chapter_set-0-title': u'',
+            'chapter_set-0-text': u'',
+            'chapter_set-1-book': book_id,
+            'chapter_set-1-number': u'',
+            'chapter_set-1-title': u'',
+            'chapter_set-1-text': u'',
+            'chapter_set-2-book': book_id,
+            'chapter_set-2-number': u'',
+            'chapter_set-2-title': u'',
+            'chapter_set-2-text': u'',
+
+            '_save': 'Save',
+        }
+
+        response = self.client.post('/admin/sample/book/%s/' % book_id, post_data)
+
+        self.assertEqual('Select book to change', response.context['title'])
