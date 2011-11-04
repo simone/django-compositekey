fb700ebfce5dc1f6c10969c2abe18aee18c2e77e
Simone Federici <s.federici@gmail.com>
2011-11-04 17:24:54 +0100

    added manager patch (on insert fields)

---
+++ src/compositekey/db/models/manager.py
@@ -0,0 +1,23 @@
+from django.db.models.manager import Manager
+
+__author__ = 'aldaran'
+from django.db.models import query as module
+
+__all__ = ["activate_insert_query_monkey_patch"]
+
+def wrap_insert_query(original_insert_query):
+
+    def _insert(self, objs, fields, **kwargs):
+        fields = [field for field in fields if not getattr(field, "not_in_db", False)]
+        print 555555, fields
+        return original_insert_query(self, objs, fields, **kwargs)
+
+    _insert._sign = "activate_insert_query_monkey_patch"
+    return _insert
+
+
+def activate_insert_query_monkey_patch():
+    # monkey patch
+    if not hasattr(Manager._insert, "_sign"):
+        print "activate_insert_query_monkey_patch"
+        Manager._insert =  wrap_insert_query(Manager._insert)
