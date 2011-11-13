8036aef0db06bfa632b3c76f0924073bf6a7b7ab
Simone Federici <s.federici@gmail.com>
2011-11-13 22:18:33 +0100

    if the class is an autocreated class, the FK contrib_to_class need to be lazy after the "creation contrib_to_class()

--- src/compositekey/db/models/fields/related.py
+++ src/compositekey/db/models/fields/related.py
@@ -1,3 +1,6 @@
+from django.db.models import signals
+import operator
+
 __author__ = 'aldaran'
 
 from django.db.models.fields.related import ForeignKey, ManyToManyRel
@@ -19,42 +22,59 @@ def wrap_fk_monkey_patch(ori_init, ori_contribute_to_class):
         ori_contribute_to_class(self, cls, name)
         if not opts.abstract:
             # STR check if is a lazy relationship
-            if not isinstance(self.rel.to, str) and getattr(self.rel.to._meta, "has_composite_primarykeys_field", False):
-                related_field = self.rel.to._meta.composite_primarykeys_field
-                opts.enable_composite = True
-                opts._prepare = wrap_meta_prepare(opts, opts._prepare)
-                opts.has_composite_foreignkeys_field = True
-                opts.composite_foreignkeys_fields = getattr(opts, "composite_foreignkeys_fields", {})
-                opts.composite_foreignkeys_fields[name]=self
-                opts.composite_special_fields = getattr(opts, "composite_special_fields", [])
-                opts.composite_special_fields.append(self)
+            def lazy_init():
+                if not isinstance(self.rel.to, str) and getattr(self.rel.to._meta, "has_composite_primarykeys_field", False):
+                    related_field = self.rel.to._meta.composite_primarykeys_field
+                    opts.enable_composite = True
+                    opts.has_composite_foreignkeys_field = True
+                    opts.composite_foreignkeys_fields = getattr(opts, "composite_foreignkeys_fields", {})
+                    opts.composite_foreignkeys_fields[name]=self
+                    opts.composite_special_fields = getattr(opts, "composite_special_fields", [])
+                    opts.composite_special_fields.append(self)
 
-                cls.__init__ = patched_model_init # adding reset PK cache
+                    cls.__init__ = patched_model_init # adding reset PK cache
 
-                new_fields = [prepare_hidden_key_field(cls, f, self.fields_ext, prefix=name) for f in related_field.get_key_fields()]
-                for f in new_fields: cls.add_to_class(f.name, f)
-                self.fields = new_fields
+                    new_fields = [prepare_hidden_key_field(cls, f, self.fields_ext, prefix=name) for f in related_field.get_key_fields()]
+                    for f in new_fields: cls.add_to_class(f.name, f)
+                    self.fields = new_fields
 
-                # get/set _id propery
-                setattr(cls, "%s_id" % name, property(get_composite_pk(new_fields, name=name), set_composite_pk(new_fields, name=name)))
+                    # get/set _id propery
+                    setattr(cls, "%s_id" % name, property(get_composite_pk(new_fields, name=name), set_composite_pk(new_fields, name=name)))
 
-                # hack add wrap setter related
-                reverse_desc = getattr(cls, name)
-                reverse_desc.__set__ = wrap_setter(reverse_desc.__set__, name, new_fields)
+                    # hack add wrap setter related
+                    reverse_desc = getattr(cls, name)
+                    reverse_desc.__set__ = wrap_setter(reverse_desc.__set__, name, new_fields)
 
-                if self.unique:
-                    names = [f.name for f in new_fields]
-                    if names not in opts.unique_together:
-                        opts.unique_together.append([f.name for f in new_fields])
+                    if self.unique:
+                        names = [f.name for f in new_fields]
+                        if names not in opts.unique_together:
+                            opts.unique_together.append([f.name for f in new_fields])
 
-                # hack db_column for joins
-                self.column = MultiColumn(new_fields)
-                self.not_in_db = True
-                self.db_type = lambda *args, **kwargs: None
-                self.db_index = False
-                self.get_prep_value = lambda self, value: value
-                self.primary_key = True
+                    # hack db_column for joins
+                    self.column = MultiColumn(new_fields)
+                    self.not_in_db = True
+                    self.db_type = lambda *args, **kwargs: None
+                    self.db_index = False
+                    self.get_prep_value = lambda self, value: value
+                    self.primary_key = True
 
+            # if the class is an autocreated class, the FK contrib_to_class need to be lazy after the "creation contrib_to_class()"
+            if opts.auto_created:
+                opts.auto_created._meta._prepare = wrap_meta_prepare(opts.auto_created._meta, opts.auto_created._meta._prepare)
+                opts.auto_created._meta._lazy_prepare_fk_actions.append(lazy_init)
+
+                def recheck_unique_together():
+                    unique_together = []
+                    for field_constraints in opts.unique_together:
+                        fields = [opts.get_field(name) for name in field_constraints]
+                        fields = [f.fields if hasattr(f, "fields") else [f] for f in fields]
+                        fields = reduce(operator.add, fields)
+                        unique_together.append([f.name for f in fields])
+                    opts.unique_together = tuple(unique_together)
+                opts.auto_created._meta._lazy_prepare_fk_actions.append(recheck_unique_together)
+
+            else:
+                lazy_init()
 
     contribute_to_class._sign = "activate_fk_monkey_patch"
     return __init__, contribute_to_class
