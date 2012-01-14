56b2e437ad4209906b1570a5ec9eba956ac16415
Simone Federici <s.federici@gmail.com>
2012-01-14 01:34:39 +0100

    setup distribute

---
+++ setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+
+setup(
+    name = "Django",
+    version = "0.1-alpha",
+    url = 'https://github.com/simone/django-compositekey',
+    author = 'Simone Federici',
+    author_email = 's.federici@gmail.com',
+    description = 'A high-level Python Web framework that encourages rapid development and clean, pragmatic design.',
+    package_dir = {"":"src/"},
+    packages = [
+        "compositekey",
+        "compositekey.db",
+        "compositekey.db.backends",
+        "compositekey.db.backends.oracle",
+        "compositekey.db.backends.postgresql_psycopg2",
+        "compositekey.db.models",
+        "compositekey.db.models.sql",
+        "compositekey.db.models.fields",
+        "compositekey.core",
+        "compositekey.core.management",
+        "compositekey.forms"
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+   ],
+)
