eb8a4c66db2493c7b46bd6a7df7916e96fd2d520
Simone Federici <sfederici@geniusbytes.com>
2012-06-30 10:11:16 +0200

    RawQuerySet fix  in this implementation, in raw query you don't need to add the primary pk, because is a composite pk, and if you do, it will be ignored.
     original issue 11
     raw()-queries require id=MultiFieldPK() to be after all other fields
    (thanks to soulne4ny)

---
+++ djangotests/composite_modeltests/composite_raw/models.py
@@ -0,0 +1,116 @@
+from django.db import models
+from compositekey.db import MultiFieldPK
+
+# id going before the model fields
+
+class CK(models.Model):
+    id = MultiFieldPK("k1", "k2")
+
+    k1 = models.CharField(max_length=100)
+    k2 = models.CharField(max_length=100)
+
+    v = models.CharField(max_length=100)
+
+    zzz = None
+
+    class Meta:
+        db_table = "ck"
+
+
+class WithAudit(models.Model):
+    created_by = models.SmallIntegerField(editable=False, null=True, blank=True)
+    created_on = models.DateTimeField(editable=False, null=True, blank=True)
+    updated_by = models.SmallIntegerField(editable=False, null=True, blank=True)
+    updated_on = models.DateTimeField(editable=False, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class CKA(WithAudit):
+    id = MultiFieldPK("k1", "k2")
+
+    k1 = models.CharField(max_length=100)
+    k2 = models.CharField(max_length=100)
+
+    v = models.CharField(max_length=100)
+
+    zzz = None
+
+    class Meta:
+        db_table = "cka"
+
+
+class CKP(CKA):
+    class Meta:
+        proxy = True
+
+    yyy = None
+
+
+# id going after the model fields
+
+class CK2(models.Model):
+    k1 = models.CharField(max_length=100)
+    k2 = models.CharField(max_length=100)
+
+    v = models.CharField(max_length=100)
+
+    id = MultiFieldPK("k1", "k2")
+
+    zzz = None
+
+    class Meta:
+        db_table = "ck2"
+
+
+class CKA2(WithAudit):
+
+    v = models.CharField(max_length=100)
+
+    k1 = models.CharField(max_length=100)
+    k2 = models.CharField(max_length=100)
+
+    id = MultiFieldPK("k1", "k2")
+
+    zzz = None
+
+    class Meta:
+        db_table = "cka2"
+
+class CKP2(CKA2):
+    class Meta:
+        proxy = True
+
+    yyy = None
+
+
+class CKN(models.Model):
+    k1 = models.CharField(max_length=100)
+    k2 = models.CharField(max_length=100)
+
+    v = models.CharField(max_length=100)
+
+    zzz = None
+
+    class Meta:
+        db_table = "ckn"
+
+
+class CKAN(WithAudit):
+
+    v = models.CharField(max_length=100)
+
+    k1 = models.CharField(max_length=100)
+    k2 = models.CharField(max_length=100)
+
+    zzz = None
+
+    class Meta:
+        db_table = "ckan"
+
+class CKPN(CKAN):
+    class Meta:
+        proxy = True
+
+    yyy = None
--- src/compositekey/db/models/manager.py
+++ src/compositekey/db/models/manager.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.db.models.manager import Manager
+from compositekey.db.models.query import RawCompositeQuerySet
 
 __author__ = 'aldaran'
 
@@ -14,13 +15,17 @@ def wrap_insert_query(original_insert_query):
         fields = [field for field in fields if not getattr(field, "not_in_db", False)]
         return original_insert_query(self, objs, fields, **kwargs)
 
+    def _raw(self, raw_query, params=None, *args, **kwargs):
+        return RawCompositeQuerySet(raw_query=raw_query, model=self.model, params=params, using=self._db, *args, **kwargs)
+
     _insert._sign = "activate_insert_query_monkey_patch"
-    return _insert
+    _insert._raw = "activate_raw_query_monkey_patch"
+    return _insert, _raw
 
 
 def activate_insert_query_monkey_patch():
     # monkey patch
     if not hasattr(Manager._insert, "_sign"):
         log.debug("activate_insert_query_monkey_patch")
-        Manager._insert =  wrap_insert_query(Manager._insert)
+        Manager._insert, Manager.raw =  wrap_insert_query(Manager._insert)
         
