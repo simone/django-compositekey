a5bc40ac353cf7f845f9bc2993f5b8632ab0ba7a
Simone Federici <sfederici@geniusbytes.com>
2012-07-05 10:06:15 +0200

    from compositekey import __future__

---
+++ testprj/testprj/settings_pg.py
@@ -0,0 +1,32 @@
+# This is an example test settings file for use with the Django test suite.
+#
+# The 'sqlite3' backend requires only the ENGINE setting (an in-
+# memory database will be used). All other backends will require a
+# NAME and potentially authentication information. See the
+# following section in the docs for more information:
+#
+# http://docs.djangoproject.com/en/dev/internals/contributing/#unit-tests
+#
+# The different databases that Django supports behave differently in certain
+# situations, so it is recommended to run the test suite against as many
+# database backends as possible.  You may want to create a separate settings
+# file for each of the backends you test against.
+
+from testprj.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME'  : 'compositekey',
+        'HOST'  : 'localhost',
+        'USER'  : 'compositekey',
+        'PASSWORD' : 'compositekey',
+    },
+    'other': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME'  : 'compositekey_other',
+        'HOST'  : 'localhost',
+        'USER'  : 'compositekey',
+        'PASSWORD' : 'compositekey',
+    }
+}
