b844b18b52dfa7a052ec3fabf5226a121604b69e
Simone Federici <sfederici@geniusbytes.com>
2012-10-26 12:19:04 +0200

    fix m2m https://github.com/simone/django-compositekey/issues/15

--- src/compositekey/utils.py
+++ src/compositekey/utils.py
@@ -1,6 +1,7 @@
 __author__ = 'aldaran'
 
 import re
+from django.db.models import Model
 P = re.compile("-('([^']+|'')*'|)")
 
 __all__ = ["assemble_pk", "disassemble_pk", "assemble_key"]
@@ -12,9 +13,10 @@ def dimention_list_generator(l, size):
     for i in xrange(size - len(l)):
         yield
 
+
 def assemble_pk(*parts):
     if None in parts:return None
-    return assemble_key(*parts)
+    return assemble_key(*[value.pk if isinstance(value, Model) else value for value in parts])
 
 def assemble_key(*parts):
     return "-".join(["'%s'" % (str(p) if not isinstance(p, (str, unicode)) else p).replace("'", "''") if p is not None else '' for p in parts])
