2a7b4c8fae2a0eecdab8c35c2731d0df63686d1d
Simone Federici <sfederici@geniusbytes.com>
2013-04-04 08:09:31 +0200

    2.5 unstable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "django-compositekey",
-    version = "0.1-alpha",
+    version = "2.5.x-unstable",
     url = 'https://github.com/simone/django-compositekey',
     author = 'Simone Federici',
     author_email = 's.federici@gmail.com',
