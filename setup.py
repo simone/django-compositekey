e6f8cacafc09b8817ae78ceee056734e4e9ec9ae
Simone Federici <sfederici@geniusbytes.com>
2014-01-26 14:30:42 +0100

    release 1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "django-compositekey",
-    version = "1.5.0-alpha",
+    version = "1.5.0",
     url = 'https://github.com/simone/django-compositekey',
     author = 'Simone Federici',
     author_email = 's.federici@gmail.com',
